989881f80b4ed66206f1c6a8c0b677c48d1a3a96
cfmack <cfmack@gmail.com>
2020-03-03 21:29:49 -0600

    added metric family sample
    And ensured build was possible

--- R/MetricFamilySample.R
+++ R/MetricFamilySample.R
@@ -2,8 +2,8 @@
 # Prometheus Raw Sample Object
 #
 #' @export
-PrometheusSample <- R6Class(
-  "PrometheusSample",
+MetricFamilySample <- R6Class(
+  "MetricFamilySample",
   public = list(
     initialize = function() {},
     getName = function() {
@@ -20,25 +20,33 @@ PrometheusSample <- R6Class(
       private$label_names = val
       invisible(self)
     },
-    getLabelValues = function() {
-      return(private$label_values)
+    getType = function() {
+      return(private$metric_type)
     },
-    setLabelValues = function(val) {
-      private$label_values = val
+    setType = function(val) {
+      private$metric_type = val
       invisible(self)
     },
-    getValue = function() {
-      return(private$sample_value)
+    getHelp = function() {
+      return(private$metric_help)
     },
-    setValue = function(val) {
-      private$sample_value = val
+    setHelp = function(val) {
+      private$metric_help = val
+      invisible(self)
+    },
+    getSamples = function() {
+      return(private$samples)
+    },
+    setSamples = function(val) {
+      private$samples = val
       invisible(self)
     }
   ),
   private = list(
     name = NULL,
     label_names = NULL,
-    label_values = NULL,
-    sample_value = NULL
+    metric_type = NULL,
+    metric_help = NULL,
+    samples = list()
   )
 )
