89b437c8865660358592c83787cb68faf47315db
cfmack <cfmack@gmail.com>
2020-03-15 19:24:10 -0500

    experimenting with travis build info

--- .build/build.R
+++ .build/build.R
@@ -6,6 +6,14 @@ library("devtools")
 library("roxygen2")
 library("renv")
 
+build_info <- function() {
+  print("***** Build Info *****")
+  print(paste("TRAVIS_BRANCH:", Sys.getenv(x="TRAVIS_BRANCH")))
+  print(paste("TRAVIS_BUILD_NUMBER:", Sys.getenv(x="TRAVIS_BUILD_NUMBER")))
+  print(paste("TRAVIS_JOB_NUMBER:", Sys.getenv(x="TRAVIS_JOB_NUMBER")))
+}
+build_info()
+
 renv::restore()
 
 devtools::document(roclets=c('rd', 'collate', 'namespace'))
