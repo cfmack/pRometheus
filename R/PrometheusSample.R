61680ee85c7cefd022f6b07b328d0ab2a7c94f6a
cfmack <cfmack@gmail.com>
2020-03-25 21:39:45 -0500

    Documenting PrometheusSample

--- R/PrometheusSample.R
+++ R/PrometheusSample.R
@@ -1,11 +1,22 @@
 ######################################################################
-#' Prometheus Raw Sample Object
+#' Prometheus Sample R6 Object
+#'
+#' Classic data object to encapsulate a sampling of a metric.   You likely will not interact witht this object
 #'
 #' @importFrom R6 R6Class
 #' @export
 PrometheusSample <- R6Class(
   "PrometheusSample",
   public = list(
+    #' Initializing PrometheusSample
+    #'
+    #' @param name name of the sample, combined in other functions or R6 objects with namespace
+    #' @param value the actual value of the sample
+    #' @param label_names the name of the labels being passed in
+    #' @param label_values the actual values of the labels.  This must be UTF-8 encoded and the same number of labels
+    #' @return instance of PrometheusSample
+    #' @examples
+    #' sample <- PrometheusSample$new("sample name", 10)
     initialize = function(name,
                           value,
                           label_names = list(),
@@ -39,22 +50,53 @@ PrometheusSample <- R6Class(
       private$label_values <- label_values
       private$sample_value <- value
     },
+
+    #' Getter for the sample name
+    #'
+    #' @return fully qualified name of the sample
+    #' @examples
+    #' sample <- PrometheusSample$new("sample name", 10)
+    #' print(sample$getName()) # 'sample name'
     getName = function() {
       return(private$name)
     },
+
+    #' Getter for the label names
+    #'
+    #' @return list of label names in the sample
     getLabelNames = function() {
       return(private$label_names)
     },
+
+    #' Getter for the label values
+    #'
+    #' @return list of label values in the sample
     getLabelValues = function() {
       return(private$label_values)
     },
+
+    #' Getter for the sample value
+    #'
+    #' @return list of sample values in the sample
     getValue = function() {
       return(private$sample_value)
     },
+
+    #' Setter for the sample value
+    #'
+    #' @param val value to actually set
+    #' @return self
     setValue = function(val) {
       private$sample_value = val
       invisible(self)
     },
+
+    #' Helper function to check if this sample has labels by checking label names
+    #'
+    #' @return bool if sample has label_names
+    #' @examples
+    #' sample <- PrometheusSample$new("sample name", 10, list("colors"), list("red"))
+    #' sample$hasLabelNames() # TRUE
     hasLabelNames = function() {
       if (is.null(private$label_names)) {
         return (FALSE)
@@ -72,3 +114,4 @@ PrometheusSample <- R6Class(
     sample_value = NULL
   )
 )
+
